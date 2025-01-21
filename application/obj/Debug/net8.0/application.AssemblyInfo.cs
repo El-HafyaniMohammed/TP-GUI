1f32d1b64fac48f7faf3d4875e1cf1e3679a082d
El-HafyaniMohammed <mohammed.elhafyani1@usmba.ac.ma>
2025-01-21 10:36:34 +0100

    rattrapage

--- application/obj/Debug/net8.0/application.AssemblyInfo.cs
+++ application/obj/Debug/net8.0/application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2002a18ea1eac14457cbe83e8221587713ade5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64003e146432e48a2eb37eadb93ba0fc20b7f8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
